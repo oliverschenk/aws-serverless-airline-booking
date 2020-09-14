a59a19588845b8469556c6d620bb910d25436001
Oliver Schenk <oliver.schenk@arcinfra.com>
2020-09-14 12:34:37 +0800

    Added default spinner

--- src/quasar.js
+++ src/quasar.js
@@ -1,13 +1,22 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import './styles/quasar.sass'
-import '@quasar/extras/material-icons/material-icons.css'
-import { Quasar } from 'quasar'
+import "./styles/quasar.sass";
+import "@quasar/extras/material-icons/material-icons.css";
+import { Quasar, Loading, QSpinnerPuff } from "quasar";
+
+// Set default loader for views
+Loading.setDefaults({
+  spinner: QSpinnerPuff,
+  spinnerSize: 200, // px
+});
 
 Vue.use(Quasar, {
   config: {},
-  components: { /* not needed if importStrategy is not 'manual' */ },
-  directives: { /* not needed if importStrategy is not 'manual' */ },
-  plugins: {
-  }
- })
+  components: {
+    /* not needed if importStrategy is not 'manual' */
+  },
+  directives: {
+    /* not needed if importStrategy is not 'manual' */
+  },
+  plugins: {},
+});
